6423a66b2b9bfa9962f3f5e78e224270ede90f6a
Denis Strashnyuk <strashnyuk.denis@gmail.com>
2022-07-20 17:20:59 +0200

    add task-24

---
+++ js/task-24.js
@@ -0,0 +1,17 @@
+/**
+
+
+Is this a triangle?
+Implement a function that accepts 3 integer values a, b, c. 
+The function should return true if a triangle can be built 
+with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+(7,2,2)
+(1,2,2)
+(2, 1, 3)
+*/
+function isTriangle(a, b, c) {
+  return a + b <= c || b + c <= a || c + a <= b ? false : true;
+}
+console.log(isTriangle(7, 2, 2));
