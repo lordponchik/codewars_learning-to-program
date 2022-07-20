de92f85552a613b60d3cd416bb68d697e309b2cd
Denis Strashnyuk <strashnyuk.denis@gmail.com>
2022-07-20 17:40:30 +0200

    add task-26

---
+++ js/task-26.js
@@ -0,0 +1,29 @@
+/**
+
+Odd or Even?
+Task:
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+
+Examples:
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"
+*/
+function oddOrEven(array) {
+  //enter code here
+  let total = 0;
+  for (const arr of array) {
+    total += arr;
+  }
+  return total % 2 === 0 ? 'even' : 'odd';
+}
+console.log(oddOrEven([0, -1, -5])); //odd
