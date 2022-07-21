e6b6567067972bad90c4cf2a751515b94e7555a1
Denis Strashnyuk <strashnyuk.denis@gmail.com>
2022-07-21 22:01:31 +0200

    add task-36

---
+++ js/task-36.js
@@ -0,0 +1,38 @@
+/**
+
+
+Two Sum
+Write a function that takes an array of numbers (integers for the tests) 
+and a target number. It should find two different items in the array that, 
+when added together, give the target value. The indices of these items should then 
+be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+For the purposes of this kata, some tests may have multiple answers; any valid solutions
+ will be accepted.
+
+The input will always be valid (numbers will be an array of length 2 or greater, and all
+   of the items will be numbers; target will always be the sum of two different items from that array).
+
+Based on: http://oj.leetcode.com/problems/two-sum/
+    [[1,2,3],          4,     [0,2]],
+    [[1234,5678,9012], 14690, [1,2]],
+    [[2,2,3],          4,     [0,1]],
+    [[2,3,1],          4,     [1,2]]
+
+twoSum([1, 2, 3], 4) // returns [0, 2] or [2, 0] */
+// complete the function
+function twoSum(numbers, target) {
+  for (let i = 0; i < numbers.length; i += 1) {
+    let firstValue = numbers[i]; //1
+    let secondValue = target - firstValue; //3
+    if (numbers.includes(secondValue) && numbers.indexOf(secondValue) !== i) {
+      return [i, numbers.indexOf(secondValue)];
+    }
+  }
+  return [];
+}
+
+console.log(twoSum([1, 2, 3], 4)); //returns [0, 2] or [2, 0]
+console.log(twoSum([1234, 5678, 9012], 14690)); //returns [1,2]
+console.log(twoSum([2, 2, 3], 4)); //[0,1]
+console.log(twoSum([2, 3, 1], 4)); //[1,2]
